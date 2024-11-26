1efc5ae52c328cf2d3ce77702a613da546823808
Sanish Kakri <Sanishkarkii3@gmail.com>
2024-11-26 17:15:45 +0545

    vayo hai

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let currentAudio = null; // Keeps track of the currently playing audio
+let currentAudio = null;
 
     function playAudio(audioFile) {
-      // Stop the currently playing audio
+
       if (currentAudio) {
         currentAudio.pause();
-        currentAudio.currentTime = 0; // Reset to the beginning
+        currentAudio.currentTime = 0;
       }
 
-      // Create and play the new audio
+
       currentAudio = new Audio(audioFile);
       currentAudio.play().then(() => {
         console.log(`Playing: ${audioFile}`);
@@ -16,7 +16,7 @@ let currentAudio = null; // Keeps track of the currently playing audio
       });
     }
 
-    // Assign buttons to play different audio files
+
     for(let i= 0; i<21; i++){
         document.getElementById(`${i+1}`).addEventListener('click', () => playAudio(`/audios/${i+1}.ogg`));
     }
