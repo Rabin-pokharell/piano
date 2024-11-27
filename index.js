237a2551c1c6d7bd51d2b5739ceefc1fa6e3fef8
Sanish Kakri <Sanishkarkii3@gmail.com>
2024-11-27 12:25:29 +0545

    update 3.2

--- index.js
+++ index.js
@@ -23,77 +23,21 @@ let currentAudio = null;
             playAudio(`/audios/${i+1}.ogg`);   
         } );
         }
+//for loop layera less code garna milne banaunu xa (popssible xa)
 
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("a")){
-          playAudio(`/audios/1.ogg`)
-          document.getElementById("1").style.backgroundColor = "hsl(0, 0%, 80%)";
-        }
-      });
 
-      document.addEventListener("keyup", event => {
-        if(event.key.startsWith("a")){
-          document.getElementById("1").style.backgroundColor = "#ffffff";
-        }
-      });
+const keyGar = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"];
 
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("s")){
-          playAudio(`/audios/2.ogg`)
-          document.getElementById("2").style.backgroundColor = "hsl(0, 0%, 80%)";
-        }
-      });
-
-      document.addEventListener("keyup", event => {
-        if(event.key.startsWith("s")){
-          document.getElementById("2").style.backgroundColor = "#ffffff";
-        }
-      });
-
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("d")){
-          playAudio(`/audios/3.ogg`)
-        }
-      });
-
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("f")){
-          playAudio(`/audios/4.ogg`)
-        }
-      });
-
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("g")){
-          playAudio(`/audios/5.ogg`)
-        }
-      });
-
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("h")){
-          playAudio(`/audios/6.ogg`)
-        }
-      });
-
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("j")){
-          playAudio(`/audios/7.ogg`)
-        }
-      });
-
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("k")){
-          playAudio(`/audios/8.ogg`)
-        }
-      });
-
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith("l")){
-          playAudio(`/audios/9.ogg`)
-        }
-      });
-
-      document.addEventListener("keydown", event => {
-        if(event.key.startsWith(";")){
-          playAudio(`/audios/10.ogg`)
-        }
-      });
+for(let i= 0; i<10; i++){
+  document.addEventListener("keydown", event => {
+    if(event.key.startsWith(`${keyGar[i]}`)){
+      playAudio(`/audios/${i+1}.ogg`)
+      document.getElementById(`${i+1}`).style.backgroundColor = "hsl(0, 0%, 80%)";
+    }
+  });
+  document.addEventListener("keyup", event => {
+    if(event.key.startsWith(`${keyGar[i]}`)){
+      document.getElementById(`${i+1}`).style.backgroundColor = "#ffffff";
+    }
+  });
+}
